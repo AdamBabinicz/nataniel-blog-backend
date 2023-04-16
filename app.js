426e46e6f46e866837a96bb19eafc19fe57cd5c7
babinicz <babinicz@onet.pl>
2023-04-16 20:36:10 +0200

    zm

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(
 app.use(
   cors({
     // origin: "http://localhost:3000",
-    origin: "blog-nataniel.netlify.app",
+    origin: "https://blog-nataniel.netlify.app",
   })
 );
 // Routes
